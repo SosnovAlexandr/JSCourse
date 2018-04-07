1a144b735ddfa085fa8af6aaca617101a5f55fb1
Alexandr <Sashejke@gmail.com>
2018-04-07 12:59:38 +0300

    12

---
+++ lesson9 Easy/js/lesson.js
@@ -0,0 +1,10 @@
+
+
+let age = document.getElementById('age');
+let somebody = new showUser('Пупкин', 'Вася');
+function showUser(surname, name) {
+	        this.value = age.value;
+         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+
+
